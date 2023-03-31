cee635bba6c9886274c14b245eae76bf99324b60
agnieve <ag@castledigital.com.au>
2023-03-31 20:49:07 +0800

    fixed animation in contact apge

--- scripts/contact_script.js
+++ scripts/contact_script.js
@@ -2,6 +2,7 @@ const tl = gsap.timeline({
     defaults: { duration: 1, ease: "Power3.easeOut" },
   });
 
+  gsap.set('.form-section', {opacity:0} )
   tl.fromTo(
     ".form-section",
     { x: "-100%", opacity: 0 },
